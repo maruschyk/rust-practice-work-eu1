07b51fbfb920b6df3ad116ed7c108ecec5951ca6
maruschyk <a.maruschyk@e-u.edu.ua>
2024-10-24 17:50:38 +0300

    Create TimeConversion_hr9.rs

---
+++ hackerrank-tests/TimeConversion_hr9.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn timeConversion(s: &str) -> String {
+
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let s = stdin_iterator.next().unwrap().unwrap();
+
+    let result = timeConversion(&s);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
