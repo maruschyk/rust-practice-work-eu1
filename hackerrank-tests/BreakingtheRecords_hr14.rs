8b51c0c33acafc8692ab373c65374ec6bcb57d67
maruschyk <a.maruschyk@e-u.edu.ua>
2024-10-24 17:56:35 +0300

    Create BreakingtheRecords_hr14.rs

---
+++ hackerrank-tests/BreakingtheRecords_hr14.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn breakingRecords(scores: &[i32]) -> Vec<i32> {
+    let mut max_count = 0;
+    let mut min_count = 0;
+
+    if scores.is_empty() {
+        return vec![0, 0]; // Handle empty scores array
+    }
+
+    let mut max_score = scores[0];
+    let mut min_score = scores[0];
+
+    for &score in scores.iter().skip(1) {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
+        }
+    }
+
+    vec![max_count, min_count]
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let scores: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = breakingRecords(&scores);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
