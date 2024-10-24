7c5cac790f52549ad19e7f42b3a94464e3250ccf
maruschyk <a.maruschyk@e-u.edu.ua>
2024-10-24 17:49:31 +0300

    Update Staircase_hr6.rs

--- hackerrank-tests/Staircase_hr6.rs
+++ hackerrank-tests/Staircase_hr6.rs
@@ -1,18 +1,10 @@
 use std::io::{self, BufRead};
 
-/*
- * Complete the 'staircase' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
 fn staircase(n: i32) {
     for i in 0..n {
-        // Calculate the number of spaces and hashes
         let spaces = " ".repeat((n - 1 - i) as usize);
         let hashes = "#".repeat((i + 1) as usize);
         
-        // Print the current row
         println!("{}{}", spaces, hashes);
     }
 }
