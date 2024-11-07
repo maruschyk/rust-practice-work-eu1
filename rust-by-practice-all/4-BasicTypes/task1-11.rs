8542b862149b425b1e86fe20925882850dd4bfe5
maruschyk <a.maruschyk@e-u.edu.ua>
2024-11-07 22:19:37 +0200

    Create task1-11.rs

---
+++ rust-by-practice-all/4-BasicTypes/task1-11.rs
@@ -0,0 +1,21 @@
+fn main() {
+    assert!(1u32 + 2 == 3);
+
+    assert!(1i32 - 2 == -1);
+    assert!(1u8.wrapping_sub(2) == 255); 
+
+    assert!(3 * 50 == 150);
+
+    assert!((9.6 / 3.2).abs() - 3.0 < f64::EPSILON); 
+
+    assert!(24 % 5 == 4);
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+}
