c297e47ddd10e2e0a992e20cbdb6b78947fe0a54
maruschyk <a.maruschyk@e-u.edu.ua>
2024-11-07 23:02:49 +0200

    Create task6-4.rs

---
+++ rust-by-practice-all/6-CompoundTypes/task6-4.rs
@@ -0,0 +1,27 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    let msgs: [Message; 3] = [
+        Message::Quit,
+        Message::Move { x: 1, y: 3 },
+        Message::ChangeColor(255, 255, 0),
+    ];
+
+    for msg in msgs {
+        show_message(msg)
+    }
+}
+
+fn show_message(msg: Message) {
+    match msg {
+        Message::Quit => println!("Quit"),
+        Message::Move { x, y } => println!("Move to ({}, {})", x, y),
+        Message::Write(text) => println!("Write: {}", text),
+        Message::ChangeColor(r, g, b) => println!("Change color to ({}, {}, {})", r, g, b),
+    }
+}
