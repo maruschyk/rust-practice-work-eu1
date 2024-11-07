021d17d43a0d2280e64d052a52306dc6dc93e784
maruschyk <a.maruschyk@e-u.edu.ua>
2024-11-07 22:55:09 +0200

    Create task2-5.rs

---
+++ rust-by-practice-all/6-CompoundTypes/task2-5.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let arr = ['a', 'b', 'c'];
+
+    // Modify this line to access the second element:
+    let ele = arr[1];
+
+    assert!(ele == 'b');
+
+    println!("Success!");
+}
