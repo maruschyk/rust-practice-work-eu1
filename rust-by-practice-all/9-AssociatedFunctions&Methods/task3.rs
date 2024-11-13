713be424ef469f3838b9c274b7f8935edd354b60
maruschyk <a.maruschyk@e-u.edu.ua>
2024-11-13 19:43:39 +0200

    Create task3.rs

---
+++ rust-by-practice-all/9-AssociatedFunctions&Methods/task3.rs
@@ -0,0 +1,17 @@
+struct TrafficLight {
+    color: String,
+}
+
+impl TrafficLight {
+    pub fn show_state(&self)  {
+        println!("the current state is {}", self.color);
+    }
+
+    pub fn change_state(&mut self) {
+        self.color = "green".to_string();
+    }
+}
+
+fn main() {
+    println!("Success!");
+}
