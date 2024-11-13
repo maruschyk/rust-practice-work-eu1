45ef98a4142fc987ddbbffb53dab32530076d554
maruschyk <a.maruschyk@e-u.edu.ua>
2024-11-13 19:27:58 +0200

    Create task6.rs

---
+++ rust-by-practice-all/7-FlowControl/task6.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // A counter variable
+    let mut n = 1;
+
+    // Loop while the condition is true
+    while n <= 10 {  // Fill the first blank
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;  // Fill the second blank to increment n
+    }
+
+    println!("n reached {}, so loop is over", n);
+}
